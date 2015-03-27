dfb6487f1b8fd5ad8ba3e363ac1023c06bde41a8
Юрій <comp9@Mac-mini-comp9.local>
2015-03-27 11:53:13 +0200

    0.0.04

---
+++ Assets/BulDestroy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnTriggerEnter2D (Collider2D one)
+	{
+		if(one.tag == "Finish")
+		{
+			Destroy(gameObject);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/ControlPlayer.cs
+++ Assets/ControlPlayer.cs
@@ -2,19 +2,17 @@
 using System.Collections;
 
 public class ControlPlayer : MonoBehaviour {
-
-	//public Transform sens;
-	//public LayerMask pol;
-	public Transform bullet;
-	private Vector2 _target;
-	public float force = 100f;
+	
+	public GameObject mBullet;
 	Rigidbody2D person1;
 	Rigidbody2D person2;
 	public bool ask;
+	int x = 1, y = 0;
 	// Use this for initialization
 	void Start () {
 		person1 = GameObject.Find("Player1").GetComponent<Rigidbody2D> ();
 		person2 = GameObject.Find("Player2").GetComponent<Rigidbody2D> ();
+
 
 	}
 	
@@ -23,22 +21,52 @@ public class ControlPlayer : MonoBehaviour {
 	{
 		if (Mathf.Abs (Input.GetAxis ("Horizontal")) > 0) {
 			person1.velocity = new Vector2 (Input.GetAxis ("Horizontal") * 10, person1.velocity.y);
+			if (Input.GetAxis ("Horizontal") > 0) {
+				x = 1;
+				y = 0;
+			} else if (Input.GetAxis ("Horizontal") < 0) {
+				x = -1;
+				y = 0;
+			}
 		}
 		if (Mathf.Abs (Input.GetAxis ("Vertical")) > 0) {
 			person1.velocity = new Vector2 (person1.velocity.x, Input.GetAxis ("Vertical") * 10);
+			if (Input.GetAxis ("Vertical") > 0) {
+				y = 1;
+				x = 0;
+			} else if (Input.GetAxis ("Vertical") < 0) {
+				y = -1;
+				x = 0;
+			}
 		}
-
 		if (Mathf.Abs (Input.GetAxis ("Horizontal")) > 0) {
 			person2.velocity = new Vector2 (Input.GetAxis ("Horizontal") * -10, person2.velocity.y);
+			if (Input.GetAxis ("Horizontal") > 0) {
+				x = 1;
+				y = 0;
+			} else if (Input.GetAxis ("Horizontal") < 0) {
+				x = -1;
+				y = 0;
+			}
 		}
 		if (Mathf.Abs (Input.GetAxis ("Vertical")) > 0) {
 			person2.velocity = new Vector2 (person2.velocity.x, Input.GetAxis ("Vertical") * 10);
+			if (Input.GetAxis ("Vertical") > 0) {
+				y = 1;
+				x = 0;
+			} else if (Input.GetAxis ("Vertical") < 0) {
+				y = -1;
+				x = 0;
+			}
 		}
 
-		if (Input.GetKey(KeyCode.Space))
+		if (Input.GetKeyDown(KeyCode.Space))
 		{
-
-			
+			GameObject bullet = (GameObject) Instantiate(mBullet, person1.transform.position , new Quaternion(0,0,0,0));
+			bullet.GetComponent<Rigidbody2D>().velocity = new Vector2 (x * 20, y * 20);
+			bullet = (GameObject) Instantiate(mBullet, person2.transform.position , new Quaternion(0,0,0,0));
+			bullet.GetComponent<Rigidbody2D>().velocity = new Vector2 (x * -20, y * 20);
+	
 		}
 	}
 
@@ -49,6 +77,11 @@ public class ControlPlayer : MonoBehaviour {
 				Destroy(gameObject);
 			}
 				
+		}
+	}
+	void OnTriggerExit2D (Collider2D Call) {
+		if (Call.name == "Exit") {
+			ask = false;
 		}
 	}
 
